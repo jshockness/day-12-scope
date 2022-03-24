31d2b69374db860e3daf1ff478ca6fc7cecaaeb5
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-24 17:48:45 +0000

    stage 2

--- main.py
+++ main.py
@@ -28,4 +28,22 @@ def drink_potion():
 drink_potion()
 print(player_health)
 
+# There is no Block Scope
+game_level = 3
+def create_enemy():
+  enemies = ["Skeleton", "Zombie", "Alien"]
+  if game_level < 5:
+    new_enemy = enemies[0]
+
+  print(new_enemy)
+
+# Modifying Global Scope
+enemies = 1
+
+def increase_enemies():
+  print(f"enemies inside function: {enemies}")
+  return enemies + 1
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
 
